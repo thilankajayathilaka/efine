62f7eeef19b74ccc5fc14491380908c0be490347
Priyasha <priyashasathyangani6@gmail.com>
2023-02-01 16:36:34 +0530

    rmv ui s added

---
+++ include/rmv_admin/navbar.php
@@ -0,0 +1,26 @@
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style3.css">
+
+        <title>Document</title>
+    </head>
+
+    <body>
+        <div class="home-content">
+            <i class='bx bx-menu'></i>
+            <div class="right-side-items">
+                <i class='bx bx-bell'></i>
+                <div class="profile">
+                    <img src="../Images/profile.png" alt="">
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+    </html>
