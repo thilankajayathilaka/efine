962eebb050cb30ad128ae9b78a38fce0b56c0195
lahiru1115 <lahirudissanayake15@gmail.com>
2023-02-08 17:12:01 +0530

    change file str

--- public/system-admin/law/law-view.php
+++ public/system-admin/law/law-view.php
@@ -10,6 +10,8 @@ require_once('../includes/functions.inc.php');
     <title>E-FINE</title>
     <link rel="stylesheet/less" type="text/css" href="../css/main.less" />
     <script src="../js/less.js" type="text/javascript"></script>
+    <script src="../js/sidebar.js"></script>
+    <script src="../js/modal.js"></script>
 </head>
 
 <body>
@@ -23,10 +25,10 @@ require_once('../includes/functions.inc.php');
         <div class="content">
             <div class="title-bar">
                 <div class="heading">
-                    <h1>RMV Admins</h1>
+                    <h1>Traffic Violation Laws</h1>
                 </div>
                 <div class="btn-group">
-                    <a href="rmv-add.php"><button class="btn btn-primary">Add New</button></a>
+                    <a href="law-add.php"><button class="btn btn-primary">Add New</button></a>
                 </div>
             </div>
             <div class="container">
@@ -35,9 +37,10 @@ require_once('../includes/functions.inc.php');
                         <thead>
                             <tr>
                                 <th>Id</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Added by</th>
+                                <th>Act</th>
+                                <th>Title</th>
+                                <th>Fine</th>
+                                <th>Points</th>
                                 <th>Timestamp</th>
                                 <th></th>
                                 <th></th>
@@ -46,18 +49,31 @@ require_once('../includes/functions.inc.php');
                         </thead>
                         <tbody>
                             <?php
-                            $result = psaView($conn);
+                            $result = lawView($conn);
                             if ($result) {
                                 while ($row = mysqli_fetch_assoc($result)) { ?>
                                     <tr>
                                         <td><?php echo $row['id']; ?></td>
-                                        <td><?php echo $row['name']; ?></td>
-                                        <td><?php echo $row['email']; ?></td>
-                                        <td><?php echo $row['addedBy']; ?></td>
+                                        <td><?php if (strlen($row['act']) > 50) {
+                                                echo substr($row['act'], 0, 50) . "...";
+                                            } else {
+                                                echo $row['act'];
+                                            } ?></td>
+                                        <td><?php if (strlen($row['title']) > 100) {
+                                                echo substr($row['title'], 0, 100) . "...";
+                                            } else {
+                                                echo $row['title'];
+                                            } ?></td>
+                                        <td><?php echo $row['fine']; ?></td>
+                                        <td><?php echo $row['points']; ?></td>
                                         <td><?php echo $row['timestamp']; ?></td>
                                         <td><i class='bx bx-detail'></i></td>
                                         <td><i class='bx bxs-edit'></i></td>
-                                        <td><i class='bx bx-trash'></i></td>
+                                        <td>
+                                            <a><i class="bx bx-trash delBtn"></i>
+                                                <?php include("delete-model.php"); ?>
+                                            </a>
+                                        </td>
                                     </tr>
                             <?php
                                 }
--- public/system-admin/rmv/rmv-view.php
+++ public/system-admin/rmv/rmv-view.php
@@ -8,10 +8,8 @@ require_once('../includes/functions.inc.php');
 
 <head>
     <title>E-FINE</title>
-    <link rel="stylesheet/less" type="text/css" href="../css/main.less" />
-    <script src="../js/less.js" type="text/javascript"></script>
-    <script src="../js/sidebar.js"></script>
-    <script src="../js/modal.js"></script>
+    <link rel="stylesheet/less" type="text/css" href="../../../css/main.less" />
+    <script type="text/javascript" src="../../../js/less.js"></script>
 </head>
 
 <body>
@@ -25,10 +23,10 @@ require_once('../includes/functions.inc.php');
         <div class="content">
             <div class="title-bar">
                 <div class="heading">
-                    <h1>Traffic Violation Laws</h1>
+                    <h1>RMV Admins</h1>
                 </div>
                 <div class="btn-group">
-                    <a href="law-add.php"><button class="btn btn-primary">Add New</button></a>
+                    <a href="rmv-add.php"><button class="btn btn-primary">Add New</button></a>
                 </div>
             </div>
             <div class="container">
@@ -37,10 +35,9 @@ require_once('../includes/functions.inc.php');
                         <thead>
                             <tr>
                                 <th>Id</th>
-                                <th>Act</th>
-                                <th>Title</th>
-                                <th>Fine</th>
-                                <th>Points</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Added by</th>
                                 <th>Timestamp</th>
                                 <th></th>
                                 <th></th>
@@ -49,28 +46,18 @@ require_once('../includes/functions.inc.php');
                         </thead>
                         <tbody>
                             <?php
-                            $result = lawView($conn);
+                            $result = psaView($conn);
                             if ($result) {
                                 while ($row = mysqli_fetch_assoc($result)) { ?>
                                     <tr>
                                         <td><?php echo $row['id']; ?></td>
-                                        <td><?php if (strlen($row['act']) > 50) {
-                                                echo substr($row['act'], 0, 50) . "...";
-                                            } else {
-                                                echo $row['act'];
-                                            } ?></td>
-                                        <td><?php if (strlen($row['title']) > 100) {
-                                                echo substr($row['title'], 0, 100) . "...";
-                                            } else {
-                                                echo $row['title'];
-                                            } ?></td>
-                                        <td><?php echo $row['fine']; ?></td>
-                                        <td><?php echo $row['points']; ?></td>
+                                        <td><?php echo $row['name']; ?></td>
+                                        <td><?php echo $row['email']; ?></td>
+                                        <td><?php echo $row['addedBy']; ?></td>
                                         <td><?php echo $row['timestamp']; ?></td>
                                         <td><i class='bx bx-detail'></i></td>
                                         <td><i class='bx bxs-edit'></i></td>
-                                        <td><a><i class='bx bx-trash' id="delBtn"></i></a></td>
-                                        <?php include("delete-model.php"); ?>
+                                        <td><i class='bx bx-trash'></i></td>
                                     </tr>
                             <?php
                                 }
