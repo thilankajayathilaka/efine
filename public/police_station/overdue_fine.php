200fe874e09aecb3b62c485d7bc05b95ba4e63a6
thilankas961 <thilankas961@gmail.com>
2023-02-08 23:55:06 +0530

    overdue fine changed

--- public/police_station/overdue_fine.php
+++ public/police_station/overdue_fine.php
@@ -25,17 +25,20 @@
         <div class="paymentsearch">
             <div class="search">
 
-                <form action="" class="searchbar">
+                <form action="" method="post">
                     <label>Search By</label>
-                    <select name="fines" id="fines" style="margin-left: 15px; width:150px;">
-                        <option value="fine_id">Fine ID</option>
+                    <select name="search_criteria" style="margin-left: 15px;">
+                        <option value="Fine_ID">Fine ID</option>
                         <option value="name">Name</option>
-                        <option value="date1">Date</option>
+                        <option value="date">Date</option>
                     </select>
-                    <input type="text" name="search" class="serchinput">
+                    <input type="text" name="search_value" class="serchinput">
                     <input type="submit" value="Search" class="searchbtn">
                     <button class="pdf">Download PDF</button>
+                    </select>
+
                 </form>
+
 
             </div>
         </div>
@@ -47,45 +50,64 @@
                     <td>Vialation</td>
                     <td>Payment status</td>
                     <td>Points</td>
-                    <td>Amount</td>
-                    <td>Overdued date</td>
+                    <td>Amount <br>(Rs)</td>
+                    <td>Overdue date</td>
                     <td>Action</td>
                 </thead>
                 <tbody>
-
                     <?php
+                    if (isset($_POST['search_value'])) {
 
-                    if ($result = mysqli_query($con, readOverdueFineDetails())) {
-                        // Fetch one and one row
+
+                        // Execute the query
+                        $result = mysqli_query($con, overduefineSearch());
+
+                        // Display the results
                         while ($row = mysqli_fetch_assoc($result)) {
-
                             $date1 = strval($row['date']);
 
                             $dateTime1 = new DateTime($date1);
                             $dateTime1->modify('14 days');
                             $date = $dateTime1->format('Y-m-d H:i:s');
-
-
                     ?>
+                    <tr>
+                        <td><?php echo $row['Fine ID']; ?></td>
+                        <td><?php echo $row['Vialation']; ?></td>
+                        <td><?php echo $row['Payment_status']; ?></td>
+                        <td><?php echo $row['Points']; ?></td>
+                        <td><?php echo $row['amount']; ?></td>
+                        <td id="data"><?php echo $date ?></td>
+                        <td><Button>Send to Court</Button></td>
+                    </tr>
+                    <?php
+                        }
+                        mysqli_free_result($result);
+                    } else {
+                        // If no search value is provided, display all the data
+                        $result = mysqli_query($con, readOverdueFineDetails());
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $date1 = strval($row['date']);
 
-                            <tr>
-                                <td><?php echo $row['Fine ID']; ?></td>
-                                <td><?php echo $row['Vialation']; ?></td>
-                                <td><?php echo $row['Payment_status']; ?></td>
-                                <td><?php echo $row['Points']; ?></td>
-                                <td><?php echo $row['amount']; ?></td>
-                                <td id="data"><?php echo $date ?></td>
-                                <td><Button>Court</Button></td>
+                            $dateTime1 = new DateTime($date1);
+                            $dateTime1->modify('14 days');
+                            $date = $dateTime1->format('Y-m-d H:i:s');
+                        ?>
+                    <tr>
+                        <td><?php echo $row['Fine ID']; ?></td>
+                        <td><?php echo $row['Vialation']; ?></td>
+                        <td><?php echo $row['Payment_status']; ?></td>
+                        <td><?php echo $row['Points']; ?></td>
+                        <td><?php echo $row['amount']; ?></td>
+                        <td id="data"><?php echo $date ?></td>
+                        <td><Button>Court</Button></td>
 
-                            </tr>
-
+                    </tr>
                     <?php
                         }
                         mysqli_free_result($result);
                     }
 
                     mysqli_close($con);
-
                     ?>
                 </tbody>
             </table>
