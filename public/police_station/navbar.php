2bc7ed64505a4e4f7484980e4da774507c016790
Priyasha <priyashasathyangani6@gmail.com>
2023-02-09 14:20:38 +0530

    added new css path

--- public/police_station/navbar.php
+++
@@ -1,9 +0,0 @@
-    <div class="home-content">
-        <i class='bx bx-menu'></i>
-        <div class="right-side-items">
-            <i class='bx bx-bell'></i>
-            <div class="profile">
-                <i class='bx bxs-user' style='color:#ffffff; font-size: 29px'></i>
-            </div>
-        </div>
-    </div>
